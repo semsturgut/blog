1549a8cc4b0e83646cb526431d50a0b049683435
Şems Turgut <sems.turgut@parasut.com>
2020-07-07 17:00:50 +0300

    Built web
    Branch: master
    Commit: c70e4c83f655bc7aa182f0530ef2ad42462a8ff4
    
    package:peanut 3.5.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "f0a814f09cfcea07e3fda64e496f4c3c",
 "/": "f0a814f09cfcea07e3fda64e496f4c3c",
-"main.dart.js": "ebc20be715401d4c6a35ea98e7a6b29d",
+"main.dart.js": "b5a098bafbfd27efa77d520ae9f58dac",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
